73a363c14e4de9631b820bf43a39c0dafdfa7214
Adryel0713 <clauber.adriel@gmail.com>
2026-02-20 15:53:03 -0300

    readonly e ?

--- index.ts
+++ index.ts
@@ -1,35 +1,17 @@
-// const person: {name: string,idade:number} = {
-//     name: "Clauber",
-//     idade: 22,
-// }
-
-// type dados = {
-//     name: string,
-//     idade: number,
-// }
-
-// const informations: dados = {
-//     name: "Clauber",
-//     idade: 22,
-// }
-
-// const inform: dados = {name: "Clauber", idade: 22};
-
-// function inf(): dados {
-//     return{
-//         name: "Clauber",
-//         idade: 22,
-//     }
-// }
-
-
 type User = {
-    nome: string,
-    idade: number,
-}
-const printUser = (user: User) => {
-    return `Nome: ${user.nome}, Idade: ${user.idade}`;
+    email?: string,
+    name: string,
+    age: number,
+    readonly cpf: string,
 }
 
-const res = printUser({nome: "Adryel",idade: 22});
-console.log(res)
+// readonly -> NUNCA PODERÁ SER MUDADO DEPOIS DE DECLARADO
+
+const Adryel: User = {
+    email: "adryel0713@gmail.com",
+    name: "Adryel",
+    age: 22,
+    cpf: "000.000.000-00"
+}
+Adryel.age = 23;
+console.log(`Email: ${Adryel.email}, name: ${Adryel.name}, age: ${Adryel.age}, cpf: ${Adryel.cpf}`)
