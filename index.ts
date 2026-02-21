4378c17ceb98ca0e7a5a9d35b93885ab9802f3b5
Adryel0713 <clauber.adriel@gmail.com>
2026-02-21 15:21:50 -0300

    OOP

--- index.ts
+++ index.ts
@@ -1,9 +1,34 @@
-enum WheatherConditions {
-    Sunny= "sunny",
-    Cloudy= "cloudy",
-    Rainy= "rainy",
-    Snowy= "snowy"
+class Animal {
+    public name: string;
+    private age: number;
+    protected species: string;
+
+    constructor(name: string,age: number, species: string){
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+
+    protected getName(): string {
+        return this.name;
+    }
+    protected getAge(): number{
+        return this.age;
+    }
+    protected getSpecies(): string{
+        return this.species;
+    }
 }
 
-const currentWheather = WheatherConditions.Cloudy
-console.log("The current wheather is " + currentWheather)
+class Dog extends Animal{
+    constructor(name: string,age: number){
+        super(name,age,"Canine")
+    }
+
+    public getInfo(): string {
+        return `${this.name} is a ${this.getSpecies()} and is ${this.getAge()} years old.`
+    }
+}
+
+const cachorro = new Dog("Pinsher",3);
+console.log(cachorro.getInfo())
