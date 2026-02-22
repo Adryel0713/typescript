4b49ee9539cd8e195163a1f127bf0d0e8b7f97b6
Adryel0713 <clauber.adriel@gmail.com>
2026-02-21 22:56:47 -0300

    interface 4

--- index.ts
+++ index.ts
@@ -1,15 +1,41 @@
-interface Conta {
-    num: string,
-    saldo: number,
-    Exibir(num: string, saldo: number): string
+interface Filme{
+    readonly nome: string,
+    avaliacao: number,
+    categoria: string,
+    Dados(): string;
 }
 
-const Adryel: Conta = {
-    num: "",
-    saldo: 0,
-    Exibir(num,saldo){
-        return `Número da conta: ${num}, saldo: ${saldo}`;
-    }
+interface Cinema extends Filme{
+    desconto?: number,
+    preco: number,
+    pago?: number,
+    status: string,
+    Valor(preco:number,pago: number): string;
 }
 
-console.log(Adryel.Exibir("4002892222",200))
+const cliente: Cinema = {
+    nome: "Harry Potter",
+    avaliacao: 8,
+    categoria: "ficção",
+    desconto: 5,
+    preco: 10,
+    status: "PENDENTE",
+    Valor(preco,pago) {
+        this.preco = preco - pago;
+        if(preco < pago){
+            this.status = "Faltou troco, espera.."
+            preco = 0;
+        }else if(preco - pago === 0){
+            this.status = "PAGO!"
+        }else{
+            this.status = "PENDENTE"
+        }
+        return this.status;
+    },
+    Dados() {
+        return `${this.nome}, ${this.avaliacao}, ${this.categoria}, ${this.desconto}, ${this.preco}, ${this.status}`
+    },
+}
+
+console.log(cliente.Valor(5,10))
+console.log(cliente.Dados())
