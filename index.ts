cf4adf87197645b23d8dbda0a178f35c474d0f83
Adryel0713 <clauber.adriel@gmail.com>
2026-02-22 17:03:03 -0300

    Generics 2

--- index.ts
+++ index.ts
@@ -1,7 +1,33 @@
-function valores<Type>(n1: Type, n2:Type): [Type,Type] {
-    return [n1,n2];
+//Gera valores e chaves aleatórias
+const sorteio = <T>(objeto: {[chave: string]: T}): {
+    chave: string;
+    valor: T;
+} => {
+    const chaves = Object.keys(objeto);
+    const random = chaves[Math.floor(Math.random() * chaves.length)];
+
+    return {
+        chave: random,
+        valor: objeto[random],
+    }
 }
 
-const num = valores<number>(2,3);
-const str = valores<string>("2","3");
-const bool = valores<boolean>(true,false);
+const numeros = {
+    n1: 5,
+    n2: 4,
+    n3: 3,
+    n4: 2,
+    n5: 1
+}
+const res = sorteio<number>(numeros)
+
+const caracteres = {
+    um: "um",
+    dois: "dois",
+    tres: "tres"
+}
+
+const res2 = sorteio<string>(caracteres)
+
+console.log(res)
+console.log(res2)
