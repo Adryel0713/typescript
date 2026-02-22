23ef8f5768d1249517b41781c76da0e9b79c34a2
Adryel0713 <clauber.adriel@gmail.com>
2026-02-21 23:02:21 -0300

    interface 5

--- index.ts
+++ index.ts
@@ -1,41 +1,22 @@
-interface Filme{
-    readonly nome: string,
-    avaliacao: number,
-    categoria: string,
-    Dados(): string;
+interface Carro {
+    placa: string,
+    Run(): void;
+}
+interface Carro {
+    placaNova: string;
+    Stop(): void;
 }
 
-interface Cinema extends Filme{
-    desconto?: number,
-    preco: number,
-    pago?: number,
-    status: string,
-    Valor(preco:number,pago: number): string;
-}
-
-const cliente: Cinema = {
-    nome: "Harry Potter",
-    avaliacao: 8,
-    categoria: "ficção",
-    desconto: 5,
-    preco: 10,
-    status: "PENDENTE",
-    Valor(preco,pago) {
-        this.preco = preco - pago;
-        if(preco < pago){
-            this.status = "Faltou troco, espera.."
-            preco = 0;
-        }else if(preco - pago === 0){
-            this.status = "PAGO!"
-        }else{
-            this.status = "PENDENTE"
-        }
-        return this.status;
+const vehicle: Carro = {
+    placa: "21A",
+    placaNova: "213ASWDAD",
+    Run() {
+        console.log("RUUUNNNNNNNN!")
     },
-    Dados() {
-        return `${this.nome}, ${this.avaliacao}, ${this.categoria}, ${this.desconto}, ${this.preco}, ${this.status}`
-    },
+    Stop() {
+        console.log("Stoped.")
+    }
 }
 
-console.log(cliente.Valor(5,10))
-console.log(cliente.Dados())
+vehicle.Run()
+vehicle.Stop()
