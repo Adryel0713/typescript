38047248aba9c3d944bab86ec9377b737cfaafff
Adryel0713 <clauber.adriel@gmail.com>
2026-02-28 16:38:16 -0300

    Generics 4

--- index.ts
+++ index.ts
@@ -1,27 +1,6 @@
-const filter = <T>(array: T[], condition: (item: T) => boolean): T[] => {
-    return array.filter((item) => condition(item));
+const reversePair = <T, U>(value1: T,value2: U): [U,T] => {
+    return [value2,value1];
 }
 
-interface fruits {
-    name: string,
-    color: string
-}
-
-const FruitArray: fruits[] = [
-    {
-        name: "Maçã",
-        color: "Vermelha"
-    },
-    {
-        name: "Banana",
-        color: "Amarela"
-    },
-    {
-        name: "Uva",
-        color: "Azul-Violeta"
-    },
-    
-]
-
-const redFruit = filter<fruits>(FruitArray, (item) => item.color === "Vermelha");
-console.log(redFruit)
+const reversedPair = reversePair("hello",20);
+console.log(reversedPair)
