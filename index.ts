ed1e21503bc36d6ea27d3e5a761851c8bbb2c36d
Adryel0713 <clauber.adriel@gmail.com>
2026-02-28 16:26:01 -0300

    Generics 3 - 2

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
-const filtrar = <T>(array: T[], condition: (condicao: T) => boolean): T[] => {
+const filter = <T>(array: T[], condition: (item: T) => boolean): T[] => {
     return array.filter((item) => condition(item));
 }
 
-const itens = [1,2,3,4,5,6,7,8,9,10]
-const itensFiltrados = filtrar<number>(itens, (item) => item % 2 === 0);
+interface fruits {
+    name: string,
+    color: string
+}
 
-console.log(itensFiltrados)
+const FruitArray: fruits[] = [
+    {
+        name: "Maçã",
+        color: "Vermelha"
+    },
+    {
+        name: "Banana",
+        color: "Amarela"
+    },
+    {
+        name: "Uva",
+        color: "Azul-Violeta"
+    },
+    
+]
+
+const redFruit = filter<fruits>(FruitArray, (item) => item.color === "Vermelha");
+console.log(redFruit)
